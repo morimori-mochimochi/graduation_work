16843b1ea2ab5ab1a3c8d96bcbcede2793d7e9e2
Morikawa chino <morikawachino@CMMBA.local>
2025-09-13 21:31:32 +0900

    駐車場から目的地までの徒歩ルートを提案

--- app/javascript/car_route.js
+++ app/javascript/car_route.js
@@ -1,23 +1,17 @@
-console.log("current_positionの読み込みが完了しました");
-
 export async function carDrawRoute(){
-  console.log("carDrawRoute開始")
   await window.mapApiLoaded;
 
   const currentPos = await new Promise ((resolve) => {
     if (window.currentPos) {
-    console.log("現在地の取得が完了しました", window.currentPos);
       resolve(window.currentPos);
     } else {
       const check = setInterval(() => {
         if (window.currentPos) {
           clearInterval(check);
-          console.log("現在地の取得が完了しました", window.currentPos)
           resolve(window.currentPos);
         }
       }, 200);
     }
-    console.log("現在地の取得が完了しました");
   });
 
   // #DirectionsAPIで使うオブジェクトの生成
@@ -28,27 +22,65 @@ export async function carDrawRoute(){
   const directionsRenderer = new google.maps.DirectionsRenderer();
   // #どのマップにルートを描画するかを指定
   directionsRenderer.setMap(window.map);
-
-  console.log("ルート描画の準備ができました");
-
-  directionsService.route(
-    {
-      origin: window.routeStart || currentPos,
-      destination: routeDestination,
-      waypoints: window.routeParking ? [{ location: window.routeParking }]: [],
-      optimizeWaypoints: true,
-      travelMode: google.maps.TravelMode.DRIVING
-    },
-    (response, status) => {
-      if (status === "OK"){
-        directionsRenderer.setDirections(response);
-        // # DirectionsResultはDirectionsServiceから返ってきた検索結果本体。ただのオブジェクトで、ルートの全情報が格納されている
-        window.directionsResult = response;
-      } else {
-        alert("ルートの取得に失敗しました: " + status);
+  
+  if (window.routeParking) {
+    //1,駐車場がある場合　出発地→駐車場（車)
+    directionsService.route(
+      {
+        origin: window.routeStart || currentPos,
+        destination: window.routeParking,
+        travelMode: google.maps.TravelMode.DRIVING
+      },
+      (response, status) => {
+        if (status === "OK"){
+          const renderer1 = new google.maps.DirectionsRenderer({
+            map: window.map,
+            polylineOptions: { strokeColor: "blue" } //車ルートは青
+          });
+          renderer1.setDirections(response);
+        } else {
+          alert("出発地→駐車場のルート取得に失敗しました: " + status);
+        }
       }
-    }
-  )
+    );
+
+    //2,駐車場→目的地(徒歩)
+    directionsService.route(
+      {
+        origin: routeParking,
+        destination: window.routeDestination,
+        travelMode: google.maps.TravelMode.WALKING
+      },
+      (response, status) => {
+        if (status === "OK") {
+          const renderer2 = new google.maps.DirectionsRenderer({
+            map: window.map,
+            polylineOptions: { strokeColor: "green" } //徒歩ルートは緑
+          });
+          renderer2.setDirections(response);
+        }else{
+          alert("駐車場→目的地のルート取得に失敗しました: " + status);
+        }
+      }
+    );
+  }else{
+    //駐車場がない場合
+    directionsService.route(
+      {
+        origin: window.routeStart || currentPos,
+        destination: window.routeDestination,
+        travelMode: google.maps.TravelMode.DRIVING
+      },
+      (response, status) => {
+        if (status === "OK") {
+          directionsRenderer.setDirections(response);
+          window.directionsResult = response;
+        } else {
+          alert("ルートの取得に失敗しました: " + status);
+        }
+      }
+    );
+  }
 };
 
 export function carRouteBtn() {
