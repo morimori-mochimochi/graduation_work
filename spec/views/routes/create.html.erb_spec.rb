0780a68a7594c90cd103a9c4b06f1290a0346378
Morikawa chino <morikawachino@CMMBA.local>
2025-10-30 11:38:02 +0900

    ribocop失敗の修正

--- spec/views/routes/create.html.erb_spec.rb
+++ spec/views/routes/create.html.erb_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-equire 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe 'routes/new.html.erb', type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
