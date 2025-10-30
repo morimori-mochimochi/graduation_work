83eacc645233c6c3c8d7a9147c4ba2c1ea0fff7c
Morikawa chino <morikawachino@CMMBA.local>
2025-10-30 21:24:02 +0900

    rubocop規約違反の修正

--- spec/system/set_parking_to_route_spec.rb
+++ spec/system/set_parking_to_route_spec.rb
@@ -20,7 +20,16 @@ RSpec.describe '駐車場を含めたルートを作成する', type: :system, j
       # evaluate_async_script: 同期的に結果を返す（JSがすぐに終わる時用)
       # evaluate_script: 非同期（promiseを使うもの)を待つ時に使う
       # 上記どちらも任意のJSを実行するメソッド
-      result = page.evaluate_async_script(<<~JS,
+      result = page.evaluate_async_script(
+        <<~JS,
+          const done = arguments[6]; // 非同期の完了を知らせるdone関数をCapybaraが最後の引数に追加
+          const[startLat, startLng, destLat, destLng, parkLat, parkLng] = arguments;
+          window.routeStart = new google.maps.LatLng({ lat: startLat, lng: startLng });
+          window.routeDestination = new google.maps.LatLng({ lat: destLat, lng: destLng });
+          window.routeParking = new google.maps.LatLng({ lat: parkLat, lng: parkLng});
+          console.log("FactoryBot data set to JS variables");
+          window.carDrawRoute().then(done).catch(e => done(e.message));
+        JS
         start.lat,
         start.lng,
         destination.lat,
@@ -28,17 +37,6 @@ RSpec.describe '駐車場を含めたルートを作成する', type: :system, j
         parking.lat,
         parking.lng
       )
-        const done = arguments[6]; // 非同期の完了を知らせるdone関数をCapybaraが最後の引数に追加
-        const[startLat, startLng, destLat, destLng, parkLat, parkLng] = arguments;
-
-        window.routeStart = new google.maps.LatLng({ lat: startLat, lng: startLng });
-        window.routeDestination = new google.maps.LatLng({ lat: destLat, lng: destLng });
-        window.routeParking = new google.maps.LatLng({ lat: parkLat, lng: parkLng});
-
-        console.log("FactoryBot data set to JS variables");
-
-        window.carDrawRoute().then(done).catch(e => done(e.message));
-      JS
 
       expect(result).to be_nil
       expect(page.evaluate_script("sessionStorage.getItem('directionsResult')")).not_to be_nil
