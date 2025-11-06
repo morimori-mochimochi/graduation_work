b6c6db67debd2c6119e3131e6b667dd4eb83a2fb
Morikawa chino <morikawachino@CMMBA.local>
2025-11-06 14:56:18 +0900

    locations_controllerのコーディング完了

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,6 @@
 class LocationsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_location, only: %i[show, edit, update, destroy]
 
     def index
         @locations = current_user.locations.order(created_at: :desc)
@@ -15,10 +16,31 @@ class LocationsController < ApplicationController
             render json: { errors: @location.errors.full_messages }, status: :unprocessable_entity
         end
     end
+
+    def show; end
+
+    def edit; end
+
+    def update
+        if @location.update(location_params)
+            redirect_to @location, notice: '場所の情報を更新しました'
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @location.destroy
+        redirect_to locations_path, notice: '削除しました', status: :see_other
+    end
     
     private
 
     def location_params
         params.require(:location).permit(:name, :lat, :lng, :address)
+    end
+
+    def set_location
+        @location = current_user.locations.find(params[:id])
     end
 end
