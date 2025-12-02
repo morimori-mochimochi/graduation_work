32d88aed854293c4f910f6799d6d5fbe9ba002c3
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-02 12:03:33 +0900

    i18n対応

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,7 +6,7 @@ class NotificationsController < ApplicationController
 
   def create
     start_datetime = @save_route.start_time
-    return redirect_to @save_route, alert: '出発時刻が設定されていません' unless start_datetime
+    return redirect_to @save_route, alert: t('.start_time_blank') unless start_datetime
 
     notify_at = start_datetime - 5.minutes
     # find_or_initialize_by: ActiveRecord のメソッドで、モデルの検索と新規オブジェクト生成を自動で行う
@@ -15,9 +15,9 @@ class NotificationsController < ApplicationController
     @notification.status = 'pending'
 
     if @notification.save
-      redirect_to @save_route, notice: "#{notify_at.strftime('%Y年%m月%d日 %H:%M')}に通知を設定しました。"
+      redirect_to @save_route, notice: t('.notice', time: notify_at.strftime('%Y年%m月%d日 %H:%M'))
     else
-      redirect_to @save_route, alert: '通知の設定に失敗しました。'
+      redirect_to @save_route, alert: t('.alert')
     end
   end
 
