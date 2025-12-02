dd6216bc1bb8aaab96e48987224c3c5139e0b734
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-02 18:54:55 +0900

    VScode消す前のコミット

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-# Railsアプリケーションの初期化が完了した後に実行する
-Rails.application.config.after_initialize do
-  require 'omniauth/oauth2'
+# ActiveSupport.run_load_hooks(:omniauth_strategy, self)これを待つ
+# OmniAuthのストラテジーが読み込まれた後に実行する
+ActiveSupport.on_load(:omniauth_strategy) do
   # OmniAuth::Strategies::OAuth2 の request_phase をオーバーライドしてログを出力
+  # class_eval:定義されているクラスに後から新しいメソッドを追加、上書き（オーバーライド）可能。
+  # 「オープンクラス」や「モンキーパッチ」とも呼ばれる。
   OmniAuth::Strategies::OAuth2.class_eval do
     def request_phase
       Rails.logger.info "OmniAuth: redirect_uri is '#{callback_url}'"
       super
     end
   end
-end
