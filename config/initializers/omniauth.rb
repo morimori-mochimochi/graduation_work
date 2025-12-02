35125123202cc50263230fe16902a1052e5b7f5c
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-02 14:48:00 +0900

    Rails初期化を待つコード追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-require 'omniauth/oauth2'
-
-# OmniAuth::Strategies::OAuth2 の request_phase をオーバーライドしてログを出力
-OmniAuth::Strategies::OAuth2.class_eval do
-  def request_phase
-    Rails.logger.info "OmniAuth: redirect_uri is '#{callback_url}'"
-    super
+# Railsアプリケーションの初期化が完了した後に実行する
+Rails.application.config.after_initialize do
+  require 'omniauth/oauth2'
+  # OmniAuth::Strategies::OAuth2 の request_phase をオーバーライドしてログを出力
+  OmniAuth::Strategies::OAuth2.class_eval do
+    def request_phase
+      Rails.logger.info "OmniAuth: redirect_uri is '#{callback_url}'"
+      super
+    end
   end
 end
