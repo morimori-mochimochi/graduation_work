f1c5e5e34577d0f2cf53923624ec599e5595a562
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-03 13:23:34 +0900

    変更点をリファクタリング

--- app/services/notification_creator.rb
+++ app/services/notification_creator.rb
@@ -14,24 +14,31 @@ class NotificationCreator
   end
 
   def call
-    execution_date = @save_route.execution_date
-    start_time = @save_route.start_time
-
     # 実行日と出発時刻の両方が設定されているか確認
-    return Result.new(false, nil, { alert: I18n.t('notifications.create.start_time_blank') }) unless execution_date && start_time
+    return Result.new(false, nil, { alert: I18n.t('notifications.create.start_time_blank') }) unless start_datetime_present?
 
-    # 日付と時刻を結合して、完全な出発日時を作成
-    start_datetime = Time.zone.local(execution_date.year, execution_date.month, execution_date.day, start_time.hour, start_time.min)
-    notify_at = start_datetime - 5.minutes # 出発の5分前に通知
     notification = @save_route.notifications.find_or_initialize_by(user: @user)
-    notification.assign_attributes(notify_at: notify_at, status: 'pending')
+    notification.assign_attributes(notify_at: calculate_notify_at, status: 'pending')
 
     if notification.save
-      message = { notice: I18n.t('notifications.create.notice', time: notify_at.strftime('%Y年%m月%d日 %H:%M')) }
-      puts "設定日時コントローラー: #{notify_at}"
+      message = { notice: I18n.t('notifications.create.notice', time: notification.notify_at.strftime('%Y年%m月%d日 %H:%M')) }
+      puts "設定日時サービス: #{notification.notify_at}"
       Result.new(true, notification, message)
     else
       Result.new(false, notification, { alert: I18n.t('notifications.create.alert') })
     end
+  end
+
+  private
+
+  def start_datetime_present?
+    @save_route.execution_date.present? && @save_route.start_time.present?
+  end
+
+  def calculate_notify_at
+    start_time = @save_route.start_time
+    execution_date = @save_route.execution_date
+    start_datetime = Time.zone.local(execution_date.year, execution_date.month, execution_date.day, start_time.hour, start_time.min)
+    start_datetime - 5.minutes # 出発の5分前に通知
   end
 end
