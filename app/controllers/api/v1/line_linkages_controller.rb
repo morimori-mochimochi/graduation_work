3176eb340745751c4f7a0ad827721537c7a54db8
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-06 20:14:24 +0900

    インデントの修正

--- app/controllers/api/v1/line_linkages_controller.rb
+++ app/controllers/api/v1/line_linkages_controller.rb
@@ -11,14 +11,14 @@ module Api
       def new
         # ログイン必須
         unless user_signed_in?
-      return render json: { error: 'LINE通知を連携するには、ログインが必要です。' }, status: :unauthorized
+          return render json: { error: 'LINE通知を連携するには、ログインが必要です。' }, status: :unauthorized
         end
 
         # 1. LINE PlatformからlinkTokenを発行してもらう
         response = line_bot_client.create_link_token(current_user.id)
         unless response.code == '200'
           Rails.logger.error "Failed to create link token: #{response.body}"
-      return render json: { error: 'LINE連携に失敗しました。もう一度お試しください。' }, status: :internal_server_error
+          return render json: { error: 'LINE連携に失敗しました。もう一度お試しください。' }, status: :internal_server_error
         end
 
         link_token = JSON.parse(response.body)['linkToken']
