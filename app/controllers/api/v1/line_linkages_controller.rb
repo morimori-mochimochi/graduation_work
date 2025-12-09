03955705d43430724cc1c432b6f0f787e380e67d
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-09 13:32:41 +0900

    リファクタリング

--- app/controllers/api/v1/line_linkages_controller.rb
+++ app/controllers/api/v1/line_linkages_controller.rb
@@ -4,10 +4,6 @@
 
 # frozen_string_literal: true
 
-require 'net/http'
-require 'uri'
-require 'json'
-
 module Api
   module V1
     # アカウント連携のためのコントローラー
@@ -20,6 +16,7 @@ module Api
       # アカウント連携ボタンクリックでnonceはDBに保存される
       # link_tokenによってユーザーはlineの画面へ移行する
       def new
+        # 1. nonceを生成し、DBに保存
         nonce = SecureRandom.urlsafe_base64
         current_user.update!(line_nonce: nonce)
 
@@ -29,15 +26,22 @@ module Api
         # issue_link_tokenAPIを呼び出した時、link_tokenが生成される
         # このlink_tokenを後のresponseに含まれるlink_tokenと比較することで安全性を検証
         # link_token: ユーザーをlineアプリのアカウント連携画面へ正しく誘導する通行手形
-        response = line_messaging_client.issue_link_token(user_id: current_user.line_login_uid)
-        # object.is_a?(Class): objectがClassまたはそのサブクラス、またはインクルードしているモジュールに属するか調べる。
-        raise unless response.is_a? (Net::HTTPOK)
+        base_url = 'https://access.line.me/oauth2/v2.1/authorize'
+        client_id = ENV['LINE_CHANNEL_ID']
 
-        link_token = JSON.parse(response.body)['linkToken']
+        params = {
+          response_type: 'code',
+          client_id: client_id,
+          redirect_uri: users_line_omniauth_callback_url, # DeviseのコールバックURL
+          state: session[:omniauth_state], # CSRF対策
+          scope: 'profile openid email',
+          nonce: nonce,
+          bot_prompt: 'aggressive'
+        }
+        auth_url = "#{base_url}?#{URI.encode_www_form(params)}"
 
-        # ユーザーをアカウント連携ページへリダイレクト
-        redirect_to "https://access.line.me/dialog/bot/accountLink?linkToken=#{link_token}"
-
+        # 3. ユーザーをLINEログイン認可画面へリダイレクト
+        redirect_to auth_url, allow_other_host: true
       rescue StandardError => e
         # 捕捉されたエラーのクラス名とメッセージをログに出力
         Rails.logger.error "LINE Account Linkage failed for user #{current_user.id}: #{e.message}"
@@ -45,15 +49,6 @@ module Api
       end
 
       private
-
-      # ApplicationControllerからline_messaging_clientを呼び出せるように。
-      # このクライアントはMessagingAPIとの通信を担当する
-      def line_messaging_client
-        @line_messaging_client ||= Line::Bot::V2::MessagingApi::ApiClient.new(
-          channel_secret: Rails.application.credentials.line[:messaging_api_secret],
-          channel_access_token: Rails.application.credentials.line[:messaging_api_channel_access_token]
-        )
-      end
     end
   end
 end
