9358b607f7865345453a8c51f5c7f5c0e0a37a69
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-12 08:57:55 +0900

    notification_mailerの記述

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,13 @@
 class NotificationMailer < ApplicationMailer
+  # 5分前を通知するメール
+  def departure_notification(notification)
+    @notification = notification
+    @save_route = notification_save_route
+    @user = notification_user
+
+    mail(
+      to: @user.email,
+      subject: "出発時刻のお知らせ: #{@save_route.name}"
+    )
+  end
 end
