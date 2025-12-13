94e3dca3426dd57c5ec18e1c3ebe427a71accb3b
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-13 14:21:01 +0900

    cronコンテナの起動失敗に対応

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,11 @@ set -e
 # Railsサーバーの古いプロセスIDファイルが残っている場合に削除
 rm -f /app/tmp/pids/server.pid
 
-# 【準備1】wheneverでcronのスケジュールを更新
-echo "Updating crontab..."
-bundle exec whenever --update-crontab
-
-# 【準備2】cronデーモンをバックグラウンドで起動
-echo "Starting cron daemon..."
-cron
+# cronコンテナの場合のみ、crontabを更新する
+if [ "$CONTAINER_ROLE" = "cron" ]; then
+  echo "Updating crontab..."
+  bundle exec whenever --update-crontab
+fi
 
 # 【メイン】DockerfileのCMDで指定されたコマンド（rails server）を実行
 echo "Starting main process..."
