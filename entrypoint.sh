5043c468451ad31f0b09510d33ac4def75a77178
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-15 12:25:32 +0900

    entrypoint.shを分岐

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,20 @@ set -e
 # Railsサーバーの古いプロセスIDファイルが残っている場合に削除
 rm -f /app/tmp/pids/server.pid
 
-# cronコンテナの場合のみ、crontabを更新する
-if [ "$CONTAINER_ROLE" = "cron" ]; then
-  echo "Updating crontab..."
-  bundle install
-  bundle exec whenever --update-crontab
-fi
+# コンテナの役割に応じて処理を分岐
+case "$CONTAINER_ROLE" in
+  "web")
+    # Webコンテナの場合、データベースを準備（作成/マイグレーション）
+    echo "Preparing database..."
+    bundle exec rails db:prepare
+    ;;
+  "cron")
+    # cronコンテナの場合、crontabを更新
+    echo "Updating crontab..."
+    bundle exec whenever --update-crontab
+    ;;
+esac
 
-# 【メイン】DockerfileのCMDで指定されたコマンド（rails server）を実行
+# DockerfileのCMDで指定されたコマンド（rails serverなど）を実行
 echo "Starting main process..."
 exec "$@"
