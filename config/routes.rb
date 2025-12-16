7262880cefcb704a85fc3bd203ae018c1bbe7db5
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-16 21:21:43 +0900

    ファイル名のタイポ修正

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
     resources :notifications, only: [:create]
   end
 
-  resource :calendar, only: [:index] do
+  resource :calendar, only: [:index]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Defines the root path route ("/")
