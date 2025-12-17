abd7705d8df6c281182efed9bc9d5eba6b399b95
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-17 12:04:38 +0900

    rubocopに対応

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,7 +6,9 @@ class CalendarsController < ApplicationController
   def index
     # pluckを使うことで、必要なカラムだけを効率的にDBから取得します。
     # これにより、不要なデータをメモリにロードすることを防ぎ、パフォーマンスが向上します。
-    events_data = current_user.save_routes.where.not(execution_date: nil).pluck(:id, :name, :start_time, :execution_date)
+    events_data = current_user.save_routes
+                              .where.not(execution_date: nil)
+                              .pluck(:id, :name, :start_time, :execution_date)
     @events = events_data.map do |id, name, start_time, execution_date|
       {
         title: "#{name} #{start_time&.strftime('%H:%M')}",
