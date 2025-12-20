48e3fd1ee57eb3bcb2cbc430199ddb8ccf2f9124
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-20 14:51:09 +0900

    プロフィール更新時にバリデエラー

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -8,10 +8,16 @@ class MypagesController < ApplicationController
   def edit; end
 
   def update
-    if current_user.update(params)
-      redirect_to mypages_path, notice: t('.notice')
+    if current_user.update_without_password(user_params)
+      redirect_to mypage_path, notice: t('.notice')
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
