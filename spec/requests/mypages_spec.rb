aa5810e8b571f1cdf2eb80fc67aa873cd60f72ad
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-21 00:08:56 +0900

    テストのログイン機能修正

--- spec/requests/mypages_spec.rb
+++ spec/requests/mypages_spec.rb
@@ -3,9 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe "Mypages", type: :request do
+  let(:user) { create(:user) }
+
+  before { sign_in user }
+
   describe "GET /show" do
     it "returns http success" do
-      get "/mypages/show"
+      get mypage_path
       expect(response).to have_http_status(:success)
     end
   end
