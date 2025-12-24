3284c2b2ee0fe00832fe52892a37dbab9e22cad8
morimori-mochimochi <reveluv.chino1205@icloud.com>
2025-12-24 13:36:36 +0900

    URL生成用のコード

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'sketto-houmon-support.com', protocol: 'https' }
+  Rails.application.routes.default_url_options = { host: 'sketto-houmon-support.com', protocol: 'https' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
