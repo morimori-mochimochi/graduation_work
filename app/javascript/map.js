d621e3fdcf3fd1052803a73eea85b44ccc4cd4b3
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-01-04 00:48:40 +0900

    現在地をマップの中心に

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,13 +1,23 @@
+import { fetchCurrentPos } from "./current_pos";
+
 await window.mapApiLoaded;
 
 export function initMap(mapDiv) {
   const gmap = new google.maps.Map(mapDiv, {
-    center: fetchCurrentPos(),
+    center: { lat: 35.681236, lng: 139.767125 }, // 初期値（例: 東京駅）を設定
     zoom: 15,
     mapId: "56e6f7b7602076fe1ca74db5"
   });
 
   // callbackとして参照されるようにグローバルへ登録
   window.map = gmap;
-}
 
+  // マップ初期化後に現在地を取得して中心を移動
+  fetchCurrentPos().then((pos) => {
+    if (pos) {
+      gmap.setCenter(pos);
+    }
+  }).catch((e) => {
+    console.warn("現在地の取得に失敗しました", e);
+  });
+}
