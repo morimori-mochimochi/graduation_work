46eebc6462e46683982b2de7b79861d91ac5d2b3
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-02-28 11:05:58 +0900

    なんらかの値が入るまで待機

--- spec/system/send_mail_spec.rb
+++ spec/system/send_mail_spec.rb
@@ -78,7 +78,7 @@ RSpec.describe '出発時刻通知メール', type: :system, js: true do
     set_route
 
     # ルート描画完了後、JSによって時刻が現在時刻に初期化されるのを待つ
-    expect(page).to have_no_select('startHour', selected: '時', wait: 10)
+    expect(page).to have_field('startHour', with: /\d+/, wait: 10)
 
     # 3. ルート描画後に時刻を設定する (値はゼロ埋めされた文字列)
     select '10', from: 'startHour'
