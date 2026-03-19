78218e3e34f6cae2f024035c1bbf9a29015eb9dd
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-03-19 13:43:57 +0900

    notification_creater.rbの追記

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,7 +5,7 @@ class NotificationsController < ApplicationController
   before_action :set_save_route
 
   def create
-    result = NotificationCreator.call(save_route: @save_route, user: current_user)
+    result = NotificationCreator.call(save_route: @save_route, user: current_user, set_to: params[:set_to])
     @notification = result.notification
     redirect_to @save_route, result.message
   end
