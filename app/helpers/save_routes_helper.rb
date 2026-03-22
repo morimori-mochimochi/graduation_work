ae8415c49b7d8dc02de6f385d8f0de418a536f64
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-03-22 09:33:39 +0900

    もう一度ngrokで

--- app/helpers/save_routes_helper.rb
+++ app/helpers/save_routes_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 module SaveRoutesHelper
+  def notification_destination_info(save_route, user)
+    notification = save_route.notifications.find_by(user: user)
+
+    if notification&.line?
+     # HTMLの<p>タグを生成
+      tag.p do
+       # concatを使うことで、複数の要素を連結して<p>タグの中に入れることができる
+        concat tag.strong("通知先LINEアカウント:")
+        concat " #{user.name}"
+      end
+    else
+      tag.p do
+        concat tag.strong("通知先メールアドレス:")
+        concat " #{user.email}"
+      end
+    end
+  end
 end
