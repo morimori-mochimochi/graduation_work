6bcee9cd47eac0e48032435a8cc275eea24fc83c
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-03-22 09:52:22 +0900

    ユーザー画面の表示分岐

--- app/helpers/mypages_helper.rb
+++ app/helpers/mypages_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module MypagesHelper
+ def mail_or_line_info(current_user)
+    if user.line_login_uid.present?
+      "LINEアカウント: #{user.name}"
+    else
+      "メールアドレス: #{user.email}"
+    end
+  end
 end
