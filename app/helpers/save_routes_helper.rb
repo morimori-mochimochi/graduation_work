41c186a2671face573cad5c15af75cf88d1437f2
morimori-mochimochi <reveluv.chino1205@icloud.com>
2026-03-22 10:32:33 +0900

    LINEログインユーザーはアカウントの修正を不可に

--- app/helpers/save_routes_helper.rb
+++ app/helpers/save_routes_helper.rb
@@ -17,4 +17,12 @@ module SaveRoutesHelper
       concat " #{value}"
     end
   end
+
+  def account_edit_button(user)
+    return if user.line_connected?
+
+    link_to edit_mypage_path do
+      image_tag "/images/user_edit.png", alt: "edit_mypage", class: "btn_effect h-17"
+    end
+  end
 end
